aa520345f702c2a060e1e84c5d3c5e2cbcca7a1f
Craig Thayer <cthayer@sazze.com>
2015-05-15 16:22:07 -0700

    version 0.1.0

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @author Craig Thayer <cthayer@sazze.com>
+ * @copyright 2015 Sazze, Inc.
+ */
+
+module.exports = {
+  Message: require('./lib/message'),
+  Response: require('./lib/response')
+};
